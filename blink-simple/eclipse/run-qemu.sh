4196f26a47877b5fadd1b566f42580c9b8be0b74
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-10-21 17:11:49 +0200

    Script updated to run the sample project on qemu. Basic Readme file added.

--- blink-simple/eclipse/run-qemu.sh
+++ blink-simple/eclipse/run-qemu.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
+echo "Run 'blink-simple' on QEMU."
+echo ""
 
-echo "Starting QEMU ARM..."
-
-/home/chm/Downloads/arm/qemu_stm32/arm-softmmu/qemu-system-arm \
+../../qemu_stm32/arm-softmmu/qemu-system-arm \
 	-M stm32-p103 \
-	-kernel /home/chm/workspace-cpp/blink-simple/target-qemu/blink-simple.elf \
+	-kernel ../target-qemu/blink-simple.elf \
 	-monitor stdio
-	# 	-clock unix \
-	#-serial stdio
-
-	# -monitor stdio
-	# -qmp tcp:localhost:4444,server 
-
-#-kernel  \
-
