3256038dca887073f048877e8e09e8e801d004ad
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-10-25 14:19:37 +0200

    Project move to the demos folder

--- blink-simple/eclipse/run-qemu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Run 'blink-simple' on QEMU."
-echo ""
-
-../../qemu_stm32/arm-softmmu/qemu-system-arm \
-	-M stm32-p103 \
-	-kernel ../target-qemu/blink-simple.elf \
-	-monitor stdio
