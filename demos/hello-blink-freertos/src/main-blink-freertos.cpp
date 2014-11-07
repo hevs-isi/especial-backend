6d3cb140c44b69667df7ed0b389a2c848d862bad
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-11-07 14:06:56 +0100

    Freertos blink demo is now a CPP file. Use the HAL to blink the led.

--- demos/hello-blink-freertos/src/main-blink-freertos.cpp
+++ demos/hello-blink-freertos/src/main-blink-freertos.cpp
@@ -1,33 +1,30 @@
-#include "helper.h"
-
 #include <stdio.h>
 #include <string.h>
+
+#include "digitaloutput.h"
+#include "helper.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
 
-int i = 0;
-char str[20] = {0};
-
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 void led_blink_task(void *pvParameters) {
+
+	// Led on the GPIOC port, pin 12
+	DigitalOutput led('C', 12);
+	led.initialize();
+	// led = 0; // Done automatically
+
 	while (1) {
-		memset(&str[0], 0, sizeof(str));
-		sprintf(str, "Blink[%03d]\n", i++);
-		send_string(str);
-		led_toggle();
-		vTaskDelay(100); // 1 second delay
+		// Toggle the led state each second
+		led.toggle();
+		vTaskDelay(100);
 	}
 }
 
 int main(void) {
-	init_led();
-
 	init_rs232();
-	enable_rs232_interrupts();
-	enable_rs232();
-
-	send_string("Hello World !\nStart blinking using FreeRTOS...\n");
+	println("Hello World !\nStart blinking using FreeRTOS...\n");
 
 	// 512 is stack size
 	xTaskCreate(led_blink_task, (signed portCHAR*)"LED Flash", 512, NULL, tskIDLE_PRIORITY + 5, NULL);
