0e57707bce8e812da8a3516bf0ab95b91222cdbb
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-12-09 14:54:57 +0100

    Demo application used to test the I/O extension board. Use 5 buttons with interrupts, 5 LEDs and 2 A/D converters. WIP.

--- demos/hello-blink/src/main.cpp
+++ demos/hello-blink/src/main.cpp
@@ -1,30 +1,66 @@
 #include "digitalinput.h"
 #include "digitaloutput.h"
+#include "analoginput.h"
 
-// Button on the GPIOC port, pin 6 (external interrupt line 6)
-DigitalInput bt1('C', 6);
+// Main board
+DigitalOutput led0('C', 12);
+DigitalInput btn0('C', 6);
+AnalogInput adc0('C', 5, 15);
 
-// Led on the GPIOC port, pin 12
-DigitalOutput led('C', 12);
+// Extension board
+//DigitalInput btn1('B', 1);
+DigitalInput btn2('C', 0);
+DigitalInput btn3('C', 1);
+DigitalInput btn4('C', 2);
 
+AnalogInput adc1('B', 0, 8);
+
+DigitalOutput led1('C', 3);
+DigitalOutput led2('C', 4);
+DigitalOutput led3('B', 8);
+DigitalOutput led4('B', 9);
+
+/**
+ * Demo application used to test the extension board.
+ *
+ * Each button is used to control the corresponding led.
+ * See the I/O extension board documentation for more details.
+ */
 int main() {
 
-	led.initialize();
-	bt1.initialize();
-	// bt1.registerInterrupt(); // Done automatically
-	// bt1.unregisterInterrupt();
+	// -- Main board --
+	led0.initialize();	// Active low
+	btn0.initialize();
+	adc0.initialize();
+
+	// -- Extension board --
+	adc1.initialize();
+
+	btn2.initialize();
+	btn3.initialize();
+	btn4.initialize();
+
+	led1.initialize();	// Active high
+	led2.initialize();
+	led3.initialize();
+	led4.initialize();
 
 	while (1) {
-		// Read the button value and updated the led.
-		// The cached value of the button is updated in the interrupt routine (ISR) -> black magic !
+		led0.set(btn0.get());
 
-		//led.set(bt1.get());
+		uint16_t value = adc1.read();
+		led1.set((value > 2048) ? true : false);
 
-		if(bt1)	// Boolean operator available
-			led.set(true);
-		else
-			led.set(false);
+		led2.set(btn2.get());
+		led3.set(btn3.get());
+		led4.set(btn4.get());
+
+		// Joystick directions values (from A/D)
+		// 0000 .. 0000  MIDDLE
+		// 0936 .. 0937	 UP
+		// 0464 .. 0465	 RIGHT
+		// 0170 .. 0171	 DOWN
+		// 1966 .. 1668  LEFT
 	}
 }
-
-// END of file 'Sch1Code.cpp'
+// END of file 'Sch1aCode.cpp'
