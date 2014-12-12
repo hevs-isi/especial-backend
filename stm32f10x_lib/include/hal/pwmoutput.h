b92324d475c7d3991291a45f0a560e87b1dc7a9c
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-12-12 17:32:39 +0100

    New PWM output. Only available for led3 and led4. Use the Timer4. Not available for all timer outputs.

---
+++ stm32f10x_lib/include/hal/pwmoutput.h
@@ -0,0 +1,38 @@
+/*
+ * PwmOutput.h
+ */
+#pragma once
+
+#include "gpio.h"
+
+#include <stdint.h>
+
+/**
+ * Configure an output as PWM mode.
+ *
+ * Available only for PB8 (TIM4_CH3) and PB9 (TIM4_CH4), respectively led3 and led4.
+ * Configure only the timer4 for now. Cannot be used with any PWM output.
+ */
+class PwmOutput: public Gpio {
+public:
+
+	PwmOutput(uint8_t port, uint8_t pin);
+	virtual ~PwmOutput();
+
+	/**
+	 * @brief Configure the pin as PWM output and set it to `OFF`.
+	 * @return `true` when configured.
+	 */
+	bool initialize();
+
+	/**
+	 * @brief Set the `ON` period of the output.
+	 * @param[in] period Period when the output is `ON`. Duty of 100% is 4096.
+	 */
+	void setPeriod(uint16_t period) const;
+
+	inline const PwmOutput & operator=(uint16_t period) {
+		setPeriod(period);
+		return *this;
+	}
+};
---
+++ stm32f10x_lib/src/hal/pwmoutput.cpp
@@ -0,0 +1,68 @@
+/*
+ * PwmOutput.cpp
+ */
+#include "pwmoutput.h"
+
+#include "stm32f10x_tim.h"
+
+PwmOutput::PwmOutput(uint8_t port, uint8_t pin) :
+		Gpio(port, pin) {
+	// Only available for PB8 and PB9 (LEDs 3 and 4)
+	assert(port == 'B');
+	assert(pin == 8 || pin == 9);
+}
+
+PwmOutput::~PwmOutput() {
+}
+
+bool PwmOutput::initialize() {
+	// Enable needed clocks
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO | gpio_port_rcc, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+
+	/* Configure the pin as push-pull PWM output. Timer is alternative function. */
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Pin = _pin.pin;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(gpio_port_base, &GPIO_InitStructure);
+
+	/* Configure timer 4. */
+	TIM_TimeBaseInitTypeDef TIM_TimeBase_InitStructure;
+	TIM_TimeBase_InitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBase_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBase_InitStructure.TIM_Period = 4096;
+	TIM_TimeBase_InitStructure.TIM_Prescaler = 71;
+	TIM_TimeBaseInit(TIM4, &TIM_TimeBase_InitStructure);
+
+	setPeriod(0); // Set to `OFF`.
+	return true;
+}
+
+void PwmOutput::setPeriod(uint16_t period) const {
+
+	if (period > 4096)
+		period = 4096;
+
+	/* Configure PWM channel output. */
+	TIM_OCInitTypeDef TIM_OC_InitStructure;
+	TIM_OC_InitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OC_InitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
+	TIM_OC_InitStructure.TIM_OCNIdleState = TIM_OCNIdleState_Set;
+	TIM_OC_InitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_OC_InitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
+	TIM_OC_InitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OC_InitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
+	TIM_OC_InitStructure.TIM_Pulse = period;
+
+	// Enable the PWM channel of the timer 4
+	if (_pin.pinNumber == 8)
+		TIM_OC3Init(TIM4, &TIM_OC_InitStructure); // Timer4, channel 3
+	else if (_pin.pinNumber == 9)
+		TIM_OC4Init(TIM4, &TIM_OC_InitStructure); // Timer4, channel 4
+	else
+		assert(false);
+
+	TIM_Cmd(TIM4, ENABLE);
+}
+
