2e0b961aa12101a37301de5207ee9930729fd9c2
Christopher Métrailler <christopher.metrailler@epfl.ch>
2014-12-23 17:03:08 +0100

    Analog input tested with multiple channels.

--- stm32f10x_lib/include/hal/analoginput.h
+++ stm32f10x_lib/include/hal/analoginput.h
@@ -9,7 +9,7 @@
 /**
  * Configure a pin as analog input.
  *
- * Use the `ADC1` for the analog to digital conversion.
+ * Use the `ADC1` for the analog to digital conversion (12 bit conversion).
  */
 class AnalogInput: public Gpio {
 public:
@@ -19,6 +19,9 @@ public:
 
 	/**
 	 * @brief Configure the pin as analog input.
+	 *
+	 * Configure the A/D module to do a 12 bit conversion on the specified channel.
+	 *
 	 * @return `true` when configured
 	 */
 	virtual bool initialize();
@@ -31,9 +34,15 @@ public:
 	}
 
 	/**
-	 * @brief Start a conversion to read the current analog value from the ADC. Save the last value as cached value.
+	 * @brief Start a conversion to read the current analog value from the ADC.
+	 * Save the last value A/D value is saved as cached value.
+	 *
+	 * @return the result of the A/D conversion (12 bit)
 	 */
 	uint16_t read();
+
+private:
+	void adInit();
 
 protected:
 	/* Analog input channel corresponding to the pin number. */
--- stm32f10x_lib/src/hal/analoginput.cpp
+++ stm32f10x_lib/src/hal/analoginput.cpp
@@ -15,19 +15,10 @@ AnalogInput::~AnalogInput() {
 
 }
 
-bool AnalogInput::initialize() {
-	GPIO_InitTypeDef gpioInitStructure;
-	ADC_InitTypeDef adcInitStructure;
-
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | gpio_port_rcc, ENABLE);
-
-	/* Configure pin as output. */
-	gpioInitStructure.GPIO_Pin = _pin.pin;
-	gpioInitStructure.GPIO_Speed = GPIO_Speed_10MHz;
-	gpioInitStructure.GPIO_Mode = GPIO_Mode_AIN;
-	GPIO_Init(gpio_port_base, &gpioInitStructure);
-
+void AnalogInput::adInit() {
 	/* A/D initialization. */
+	ADC_InitTypeDef adcInitStructure;
+	ADC_DeInit(ADC1);
 	ADC_StructInit(&adcInitStructure);
 	adcInitStructure.ADC_Mode = ADC_Mode_Independent;
 	adcInitStructure.ADC_ScanConvMode = DISABLE;
@@ -36,13 +27,28 @@ bool AnalogInput::initialize() {
 	adcInitStructure.ADC_DataAlign = ADC_DataAlign_Right;
 	adcInitStructure.ADC_NbrOfChannel = 1;
 	ADC_Init(ADC1, &adcInitStructure);
+	ADC_Cmd(ADC1, ENABLE); // Enable the ADC
+}
 
-	// Enable the ADC
-	ADC_Cmd(ADC1, ENABLE);
+bool AnalogInput::initialize() {
+	GPIO_InitTypeDef gpioInitStructure;
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | gpio_port_rcc, ENABLE);
+
+	/* Configure pin as output. */
+	gpioInitStructure.GPIO_Pin = _pin.pin;
+	gpioInitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	GPIO_Init(gpio_port_base, &gpioInitStructure);
+
+	adInit();
 	return true;
 }
 
 uint16_t AnalogInput::read() {
+
+	// FIXME: before changing the channel, the A/D must be initialized again.
+	adInit();
+
 	// Configure channel
 	ADC_RegularChannelConfig(ADC1, _channel, 1, ADC_SampleTime_55Cycles5);
 
@@ -55,5 +61,6 @@ uint16_t AnalogInput::read() {
 
 	// Get the conversion value and save it as cached value
 	_lastValue = ADC_GetConversionValue(ADC1);
+	ADC_SoftwareStartConvCmd(ADC1, DISABLE);
 	return _lastValue;
 }
