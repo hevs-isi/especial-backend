5dedd35f9e6ea299021bcac574828a14b4938a71
Christopher Métrailler <christopher.metrailler@epfl.ch>
2015-01-16 12:27:29 +0100

    Pulse counter component. Use external interrupts to count pulses (on rising and falling edges).

---
+++ stm32f10x_lib/include/demo/pulseinput.h
@@ -0,0 +1,54 @@
+/*
+ * PulseInput.h
+ */
+#pragma once
+
+#include "digitalinput.h"
+#include "utils/time.h"
+
+/**
+ * Configure the input as a digital input.
+ * Registered as an external interrupt to count the number of edges.
+ */
+class PulseInput: public DigitalInput {
+public:
+
+	PulseInput(uint8_t port, uint8_t pin);
+	virtual ~PulseInput();
+
+	/**
+	 * Time and input initialization.
+	 *  @return `true` if success, `false` if the interrupt registration fails
+	 */
+	virtual bool initialize();
+
+	/** Hardware interrupt for each rising and falling edge. */
+	virtual void irq();
+
+	/**
+	 * Return the last valid time measured.
+	 * Correspond to the time of 4 pulse captured from the fan.
+	 *
+	 * @return the time measured between 8 edges (4 pulses)
+	 */
+	inline uint32_t getSpeed() {
+		return lastPulseTime;
+	}
+
+	inline uint32_t get() const {
+		return lastPulseTime;
+	}
+
+private:
+	// Start time of the counter.
+	timeout_t startTime;
+
+	// Current time value when an edge is detected.
+	timeout_t currentTime;
+
+	// Number of valid pulse detected (pulse signal periods)
+	uint32_t pulseCounter;
+
+	// Time measure of the last valid pulse.
+	uint32_t lastPulseTime;
+};
---
+++ stm32f10x_lib/src/demo/pulseinput.cpp
@@ -0,0 +1,45 @@
+/*
+ * PulseInput.cpp
+ */
+#include "pulseinput.h"
+
+PulseInput::PulseInput(uint8_t port, uint8_t pin) :
+		DigitalInput(port, pin) {
+	pulseCounter = 0;
+	lastPulseTime = 1000; // Fan is "OFF" at start
+}
+
+PulseInput::~PulseInput() {
+}
+
+bool PulseInput::initialize() {
+	bool res = DigitalInput::initialize();
+	time_init();
+	time_resume();
+	return res;
+}
+
+void PulseInput::irq() {
+	if (pulseCounter == 0) {
+		startTime = time_get();
+	}
+
+	// Measure the elapsed time after 4 pulses (8 edges)
+	else if (pulseCounter == 8) {
+		timeout_t now = time_get();
+		lastPulseTime = time_diff_ms(now, startTime);
+
+		// Reset the counter and count the next 8 edges
+		startTime = time_get();
+		pulseCounter = 0;
+	}
+
+	// Software glitch filter. Fix a minimum of ms between two edges.
+	timeout_t n = time_get();
+	uint32_t pulseWidth = time_diff_ms(n, currentTime);
+	if (pulseWidth >= 12) {
+		// Count pulse if not a glitch (> 12ms between edges)
+		currentTime = time_get();
+		pulseCounter += 1;
+	}
+}
